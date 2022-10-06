7e579ef89df3e9c867e0df9d398d67a885c58f78
Greg Poslov <grig.skamsk@mail.ru>
2022-10-06 20:04:29 +0500

    Corrected

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,13 +1,5 @@
-import java.util.Scanner;
-
 public class BmiService {
-    public float calculate() {
-        Scanner bw = new Scanner(System.in); //Сканер массы тела
-        System.out.println("Масса тела в кг:");
-        int bodyWeight = bw.nextInt(); // Масса тела
-        Scanner g = new Scanner(System.in); //Сканер роста
-        System.out.println("Рост в см:");
-        double growth = g.nextInt(); // Рост
+    public float calculate(float bodyWeight, double growth) {
         float bmi = (float) (bodyWeight / ((growth * growth) / 10000));
         return bmi;
     }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        float coutingBmi = service.calculate();
+        Scanner bw = new Scanner(System.in); //Сканер массы тела
+        System.out.println("Масса тела в кг:");
+        int bodyWeight = bw.nextInt(); // Масса тела
+        Scanner g = new Scanner(System.in); //Сканер роста
+        System.out.println("Рост в см:");
+        float growth = g.nextInt(); // Рост
+        float coutingBmi = service.calculate(bodyWeight, growth);
         System.out.printf("Ваш ИМТ = %.1f", coutingBmi);
     }
 }
